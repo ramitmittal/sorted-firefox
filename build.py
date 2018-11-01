7569ce8f95a7a1a6e70ae93da60cef664972e308
Ramit Mittal <ramit1216258@jmit.ac.in>
2018-11-01 13:53:43 +0530

    build script checks python version
    Signed-off-by: Ramit Mittal <ramit1216258@jmit.ac.in>

--- build.py
+++ build.py
@@ -1,6 +1,12 @@
 import os
 import shutil
 import subprocess
+import sys
+
+if sys.version_info < (3,5):
+    print("Need python3 to build. Exiting")
+    sys.exit(1)
+
 
 try:
     shutil.rmtree("dist/")
