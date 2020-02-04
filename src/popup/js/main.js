9b625dd6b92fb614def4dcf2d150fcc9297d1624
Ramit Mittal <ramit1216258@jmit.ac.in>
2020-02-04 11:53:21 +0530

    refactor popup js

--- src/popup/js/main.js
+++ src/popup/js/main.js
@@ -1,5 +1,7 @@
 import '../custom.sass';
 
+import * as fp from 'lodash/fp';
+
 // eslint-disable-next-line no-undef
 const { bookmarks } = browser;
 
@@ -14,115 +16,102 @@ const sortInput = document.getElementById('sort-input');
 const unsortDialog = document.getElementById('unsort-dialog');
 const unsortAccept = document.getElementById('unsort-accept');
 const unsortReject = document.getElementById('unsort-reject');
-const currentSortedFolders = []; // contains full bookmarkTreeNode objects
+
 const newSortedFolders = []; // contains only titles of bookmark folders to be created
+const deletedSortedFolders = []; // contains only title of bookmark folders marked for deletion
 
 /**
- * Empty the existing sorted folders, moving all child bookmarks out. Then delete the existing sorted folders.
+ * Performs the main bookmark sort and move function
+ * @returns {Promise<void>}
+ */
+async function go() {
+  // 1.1 create new sorted folders
+  const bookmarkFolderCreationPromises = newSortedFolders
+    .map(item => bookmarks.create({
+      title: `${item} (sorted)`,
+      type: 'folder',
+    }));
+  await Promise.all(bookmarkFolderCreationPromises);
+
+  // 1.2 fetch all sorted folders
+  const currentSortedFolders = (await bookmarks.search({ query: '(sorted)' }))
+    .filter(item => item.title.endsWith('(sorted)') && item.type === 'folder')
+    .filter(item => !deletedSortedFolders.includes(item));
+
+  // 1.3 get ids of default bookmarks folders
+  const allBookmarks = await bookmarks.search({});
+  const defaultFolderParentIds = fp.compose(
+    fp.map(item => item.id),
+    fp.take(5),
+  )(allBookmarks);
+
+  // 1.4 user created bookmark folders are not to be touched
+  const candidatesForSorting = fp.compose(
+    fp.filter(item => item.type !== 'folder' && defaultFolderParentIds.includes(item.parentId)),
+    fp.drop(5),
+  )(allBookmarks);
+
+  // 1.5 get sortedFolderTags to aid in sorting
+  const sortedFolderTags = currentSortedFolders.map((item) => {
+    const tags = item.title.toLowerCase()
+      .slice(0, item.title.indexOf('(sorted)'))
+      .split(',')
+      .map(s => s.trim());
+    return [tags, item.id];
+  });
+
+  const bookmarksMovePromises = candidatesForSorting.map((item) => {
+    const titleTokens = item.title
+      .toLowerCase()
+      .split(/[, .?!:]/)
+      .map(s => s.trim());
+
+    for (const token of titleTokens) {
+      for (const sortTag of sortedFolderTags) {
+        if (sortTag[0].includes(token)) return bookmarks.move(item.id, { parentId: sortTag[1] });
+      }
+    }
+    return null;
+  });
+  await Promise.all(bookmarksMovePromises);
+}
+
+/**
+ * Empty the existing sorted folders, moving all child bookmarks out.
+ * Then delete the existing sorted folders.
  * @returns {Promise<void>}
  */
 async function flattenAll() {
   const allBookmarks = await bookmarks.search({});
   const otherBookmarksId = allBookmarks[3].id;
-  const existingSortedFolderIds = [];
-  allBookmarks.forEach((item) => {
-    if (item.title.endsWith('(sorted)') && item.type === 'folder') {
-      existingSortedFolderIds.push(item.id);
-    }
-  });
-  let allPromises = allBookmarks.map((item) => {
-    if (existingSortedFolderIds.includes(item.parentId)) {
-      return bookmarks.move(item.id, { parentId: otherBookmarksId });
-    }
-    return null;
-  });
-  await Promise.all(allPromises);
-  allPromises = existingSortedFolderIds.map(item => bookmarks.remove(item));
-  await Promise.all(allPromises);
-  // currentSortedFolders.splice(0, currentSortedFolders.length);
-  window.close();
+  const existingSortedFolderIds = allBookmarks
+    .filter(item => item.title.endsWith('(sorted)') && item.type === 'folder')
+    .map(item => item.id);
+
+
+  const bookmarkMovePromises = allBookmarks
+    .filter(item => existingSortedFolderIds.includes(item.parentId))
+    .map(item => bookmarks.move(item.id, { parentId: otherBookmarksId }));
+  await Promise.all(bookmarkMovePromises);
+
+  const deletedSortedFoldersPromises = existingSortedFolderIds
+    .map(item => bookmarks.remove(item));
+  await Promise.all(deletedSortedFoldersPromises);
 }
 
-async function go() {
-  const createdSortedFolders = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const x of newSortedFolders) {
-    // eslint-disable-next-line no-await-in-loop
-    const newBookmarkFolder = await bookmarks.create({
-      title: `${x} (sorted)`,
-      type: 'folder',
-    });
-    createdSortedFolders.push(newBookmarkFolder);
-  }
-  // We will move bookmarks inside these folders
-  const sortHeaders = [...createdSortedFolders, ...currentSortedFolders];
 
-  let allBookmarks = await bookmarks.search({});
-  // Do not touch the default folders
-  const defaultFolders = allBookmarks.splice(0, 5);
-  // Do not touch a user created folder or any bookmark inside a user created folder.
-  allBookmarks = allBookmarks.filter((item) => {
-    if (item.type === 'folder') return false;
-    // eslint-disable-next-line no-restricted-syntax
-    for (const x of defaultFolders) {
-      if (item.parentId === x.id) return true;
-    }
-    return false;
-  });
-  const finalPromises = [];
-  sortHeaders.forEach((header) => {
-    const tags = header.title.toLowerCase()
-      .slice(0, header.title.indexOf('(sorted)'))
-      .split(',')
-      .map(s => s.trim());
-    // eslint-disable-next-line no-restricted-syntax
-    for (const x of allBookmarks) {
-      const title = x.title.toLowerCase()
-        .split(/[, .?!:]/)
-        .map(y => y.trim());
-      // eslint-disable-next-line no-restricted-syntax
-      for (const z of title) {
-        if (tags.includes(z)) finalPromises.push(bookmarks.move(x.id, { parentId: header.id }));
-      }
-    }
-  });
-  await Promise.all(finalPromises);
-  window.close();
-}
+// Helpers for managing basic UI changes
+const hideElement = element => element.setAttribute('hidden', true);
+const showElement = element => element.removeAttribute('hidden');
+const hideMainDialog = fp.wrap(hideElement, mainDialog);
+const showUnsortDialog = fp.wrap(showElement, unsortDialog);
+const showSortDialog = fp.wrap(showElement, sortDialog);
 
-async function getCurrentSortedFolders() {
-  const results = await bookmarks.search({ query: '(sorted)' });
-  currentSortedFolders.push(...results);
-}
-
-function sortListDelete(event) {
-  const item = event.target.parentNode.innerText;
-  const filtered = currentSortedFolders.filter(entry => entry.title === item);
-  if (filtered.length > 0) {
-    currentSortedFolders.splice(currentSortedFolders.indexOf(filtered[0]), 1);
-  } else {
-    newSortedFolders.splice(newSortedFolders.indexOf(item), 1);
-  }
-  sortList.removeChild(event.target.parentNode);
-}
-
-function populateSortDialog() {
-  currentSortedFolders.forEach((item) => {
-    const tempA = document.createElement('a');
-    tempA.classList.add('dropdown-item');
-    const tempSpan = document.createElement('span');
-    tempSpan.classList.add('delete', 'is-small');
-    tempSpan.style.marginRight = '10px';
-    tempSpan.style.top = '3px';
-    tempSpan.addEventListener('click', sortListDelete);
-    const tempText = document.createTextNode(item.title);
-    tempA.appendChild(tempSpan);
-    tempA.appendChild(tempText);
-    sortList.insertBefore(tempA, sortList.firstChild);
-  });
-}
-
-function addNewSortItem(item) {
+/**
+ * Create new element in sorted folders list.
+ * @param {String} itemText - text of element
+ */
+function createNewSortListElement(itemText) {
   const tempA = document.createElement('a');
   tempA.classList.add('dropdown-item');
   const tempSpan = document.createElement('span');
@@ -130,52 +119,89 @@ function addNewSortItem(item) {
   tempSpan.style.marginRight = '10px';
   tempSpan.style.top = '3px';
   tempSpan.addEventListener('click', sortListDelete);
-  const tempText = document.createTextNode(item);
+  const tempText = document.createTextNode(itemText);
   tempA.appendChild(tempSpan);
   tempA.appendChild(tempText);
   sortList.insertBefore(tempA, sortInput.parentElement);
 }
 
+
 /**
- * Handle the click of the 'new' button in sort dialog.
+ * This event listener is attached to the "X" span element of each list item.
+ * @param event - HTML event
  */
-function handleNew() {
-  if (sortInput.value.trim() !== '') {
-    const tagArray = sortInput.value.trim()
-      .split(',')
-      .map(item => item.toLowerCase()
-        .trim());
-    const tagString = tagArray.join(', ');
-    newSortedFolders.push(tagString);
-    addNewSortItem(tagString);
-    sortInput.value = '';
+function sortListDelete(event) {
+  const itemTitle = event.target.parentNode.innerText;
+  if (newSortedFolders.includes(itemTitle)) {
+    newSortedFolders.splice(newSortedFolders.indexOf(itemTitle), 1);
+  } else {
+    deletedSortedFolders.push(itemTitle);
   }
+  sortList.removeChild(event.target.parentNode);
 }
 
+function populateSortDialog() {
+  bookmarks.search({ query: '(sorted)' })
+    .then(results => results
+      .map(item => createNewSortListElement(item.title)));
+}
+
+function addNewSortedFolder() {
+  if (sortInput.value.trim() === '') return;
+
+  const tagArray = sortInput.value
+    .trim()
+    .split(',')
+    .map(item => item
+      .toLowerCase()
+      .trim());
+
+  const tagString = tagArray.join(', ');
+
+  newSortedFolders.push(tagString);
+  createNewSortListElement(tagString);
+  sortInput.value = '';
+}
+
+
+// Initialize all event listeners for the popup.
 function initListeners() {
-  mainSortLink.addEventListener('click', () => {
-    mainDialog.setAttribute('hidden', 'true');
-    sortDialog.removeAttribute('hidden');
-    getCurrentSortedFolders()
-      .then(() => populateSortDialog());
-  });
-  mainUnsortLink.addEventListener('click', () => {
-    mainDialog.setAttribute('hidden', 'true');
-    unsortDialog.removeAttribute('hidden');
-  });
-  unsortAccept.addEventListener('click', () => {
-    mainDialog.removeAttribute('hidden');
-    unsortDialog.setAttribute('hidden', 'true');
-    flattenAll();
-  });
+  mainSortLink.addEventListener(
+    'click',
+    () => {
+      populateSortDialog();
+      showSortDialog();
+      hideMainDialog();
+    },
+  );
+  mainUnsortLink.addEventListener(
+    'click',
+    () => {
+      showUnsortDialog();
+      hideMainDialog();
+    },
+  );
+
+  unsortAccept.addEventListener(
+    'click',
+    async () => {
+      await flattenAll();
+      window.close();
+    },
+  );
   unsortReject.addEventListener('click', () => {
-    mainDialog.removeAttribute('hidden');
-    unsortDialog.setAttribute('hidden', 'true');
+    window.close();
   });
-  sortNew.addEventListener('click', handleNew);
-  sortGo.addEventListener('click', go);
+
+  sortNew.addEventListener('click', addNewSortedFolder);
+
+  sortGo.addEventListener(
+    'click',
+    async () => {
+      await go();
+      window.close();
+    },
+  );
 }
 
-window.onload = () => {
-  initListeners();
-};
+window.onload = initListeners;
